414b241c6192091168ee8de2ff52e5609f015736
gramatan <78417563+gramatan@users.noreply.github.com>
2023-06-15 09:45:18 +0600

    dice roller update. next game info(set, game) add

--- func.py
+++ func.py
@@ -1,29 +1,36 @@
 import re
 import random
 
-
+#
 # def roll_dice(expression):
 #     parts = re.findall(r'([+-]?)(\d*)d(\d+)?|([+-]\d+)', expression)
 #     rolls = []
 #     total = 0
-#     print(parts)
 #     for part in parts:
 #         sign, dice, sides, const = part
 #         sign = -1 if sign == '-' else 1
-#         if dice:
+#         if dice or sides:
 #             dice = int(dice) if dice else 1
-#             sides = int(sides) if sides else 1  # Default to 1 if no sides specified (e.g., "+5" or "-5")
+#             sides = int(sides) if sides else 1
 #             for _ in range(dice):
 #                 roll = sign * random.randint(1, sides)
 #                 rolls.append(str(roll))
 #                 total += roll
-#         if const:  # If there is a constant modifier, add or subtract it from the total
+#         if const:
 #             rolls.append(f'{sign * int(const)}')
 #             total += sign * int(const)
 #     return f'{"+".join(rolls)} = {total}' if len(rolls) > 1 else f'{total}'
 
+import re
+import random
+
+
 def roll_dice(expression):
-    parts = re.findall(r'([+-]?)(\d*)d(\d+)?|([+-]\d+)', expression)
+    # Prevent huge numbers
+    if any(int(num) > 100 for num in re.findall(r'\d+', expression)):
+        return '¯\_(ツ)_/¯'
+
+    parts = re.findall(r'([+-]?)(\d*)[dD](\d+)?|([+-]\d+)', expression, re.IGNORECASE)
     rolls = []
     total = 0
     for part in parts:
@@ -31,12 +38,13 @@ def roll_dice(expression):
         sign = -1 if sign == '-' else 1
         if dice or sides:
             dice = int(dice) if dice else 1
-            sides = int(sides) if sides else 1
+            sides = int(sides) if sides else 1  # Default to 1 if no sides specified (e.g., "+5" or "-5")
             for _ in range(dice):
                 roll = sign * random.randint(1, sides)
                 rolls.append(str(roll))
                 total += roll
-        if const:
-            rolls.append(f'{sign * int(const)}')
+        if const:  # If there is a constant modifier, add or subtract it from the total
             total += sign * int(const)
+            rolls.append(str(sign * int(const)))
     return f'{"+".join(rolls)} = {total}' if len(rolls) > 1 else f'{total}'
+
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import logging
 import re
-import random
 from aiogram import Bot, Dispatcher, types
 
 from func import roll_dice
 from config import TOKEN
+import sqlite3
+
+conn = sqlite3.connect('dnd_bot.db')  # Creates a new db file if it doesn't exist
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS games
+             (chat_id INTEGER PRIMARY KEY, game_info TEXT)''')
+
+conn.commit()
+conn.close()
 
 API_TOKEN = TOKEN
 
@@ -13,17 +23,69 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi! I'm a dice rolling bot. Send me a message like '1d20' or '2d4+2' to roll dice.")
+    welcome_message = (
+        "Привет! Я бот, который может помочь в ваших DnD приключениях, бросая кости за вас. "
+        "Просто отправьте сообщение в формате '/NdM+K', где N - количество кубиков, "
+        "M - количество граней на кубике, K - модификатор (необязательно). Например, '/2d20+5'. "
+        "Я вычислю результат для вас!\n\n"
+        "Вы также можете установить информацию о следующей игре с помощью команды /set, "
+        "а потом получить ее обратно с помощью команды /game.\n\n"
+        "Вот список сайтов, которые могут помочь:\n"
+        "[DnD.su. Справочник по заклинаниям](https://dnd.su/spells/)\n"  
+        "[DnD.su. Справочник по классам](https://dnd.su/class/)\n"
+        "[DnD.su. Книги и руководства](https://dnd.su/books/)\n\n"
+        "[Интерактивный чарлист](https://longstoryshort.app/characters/list/)\n\n"
+        "[Правила для начинающих игроков](https://www.dungeonsanddragons.ru/bookfull/5ed/5e%20starter%20set%20-%20basic%20rules%20RUS.pdf)\n"
+        "[Миниатюры персонажей Hero Forge](https://www.heroforge.com/)\n"
+    )
+
+    await message.reply(welcome_message, parse_mode=types.ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
+@dp.message_handler(commands=['set'])
+async def set_game(message: types.Message):
+    chat_id = message.chat.id
+    game_info = message.text[5:]  # Removes the '/set ' part
+
+    conn = sqlite3.connect('dnd_bot.db')
+    c = conn.cursor()
+
+    # Insert or update game_info
+    c.execute('''INSERT OR REPLACE INTO games (chat_id, game_info)
+                 VALUES (?, ?)''', (chat_id, game_info))
+
+    conn.commit()
+    conn.close()
+
+    await message.reply("Информация о следующей партии сохранена")
+
+
+@dp.message_handler(commands=['game'])
+async def get_game(message: types.Message):
+    chat_id = message.chat.id
+
+    conn = sqlite3.connect('dnd_bot.db')
+    c = conn.cursor()
+
+    # Retrieve game_info
+    c.execute("SELECT game_info FROM games WHERE chat_id = ?", (chat_id,))
+    game_info = c.fetchone()
+
+    conn.close()
+
+    if game_info is None:
+        await message.reply("Информации о следующей игре нет. может быть вы забыли о команде /set?")
+    else:
+        await message.reply(game_info[0])
 
 
 @dp.message_handler()
 async def dice_roll(message: types.Message):
-    requests = re.findall('([+-]?\d*d\d+[+-]?\d*|[+-]\d+)', message.text)
+    requests = re.findall(r'/(.*?)(?=\s|$|[^0-9dD+-])', message.text, re.IGNORECASE)
     if requests:
-        results = [f'{request}: {roll_dice(request)}' for request in requests]
+        results = [f'/{request}: {roll_dice(request)}' for request in requests]
         await message.reply('\n'.join(results))
 
 
