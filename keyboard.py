e2df012984edb1c84362b102c3f8ba8d2356627c
gramatan <78417563+gramatan@users.noreply.github.com>
2023-06-18 01:37:33 +0600

    char creation done.

--- keyboard.py
+++ keyboard.py
@@ -4,7 +4,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def character_creation_keyboard():
     keyboard = InlineKeyboardMarkup(row_width=2)
     keyboard.add(
-        InlineKeyboardButton('Пресет', callback_data='select_preset'),
+        InlineKeyboardButton('Пресет', callback_data='toggle_list'),
         InlineKeyboardButton('Класс', callback_data='select_class'),
         InlineKeyboardButton('Раса', callback_data='select_race'),
         InlineKeyboardButton('Предыстория', callback_data='select_story'),
