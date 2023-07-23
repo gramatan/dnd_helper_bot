51fc3daf4c7f8aaa7c4cb79b0682d6636102eb1b
gramatan <78417563+gramatan@users.noreply.github.com>
2023-07-23 22:46:03 +0600

    Add commented logging level switch

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from config import TOKEN
 API_TOKEN = TOKEN   # your telegram bot token
 
 # Initialize bot and dispatcher
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
