c0cc2d49d1e2cf0da8664c4c144d09b92f2f416a
gramatan <78417563+gramatan@users.noreply.github.com>
2023-08-02 14:46:41 +0600

    Added context manager to manage database connections. Updated functions to use the new database context manager

--- database/db.py
+++ database/db.py
@@ -1,23 +1,33 @@
 import logging
+
 import sqlite3
+from contextlib import contextmanager
+
+
+@contextmanager
+def database_connection(commit=False):
+    conn = sqlite3.connect('./db/dnd_bot.db')
+    cursor = conn.cursor()
+    try:
+        yield cursor
+        if commit:
+            conn.commit()
+    finally:
+        conn.close()
 
 
 def create_if_not_exist():
     try:
-        conn = sqlite3.connect('./db/dnd_bot.db')  # Creates a new db file if it doesn't exist
-        c = conn.cursor()
+        with database_connection(commit=True) as cursor:
+            # Create table games
+            cursor.execute('''CREATE TABLE IF NOT EXISTS games
+                         (chat_id INTEGER PRIMARY KEY, game_info TEXT)''')
 
-        # Create table games
-        c.execute('''CREATE TABLE IF NOT EXISTS games
-                     (chat_id INTEGER PRIMARY KEY, game_info TEXT)''')
-        conn.commit()
-
-        # Create table logs
-        c.execute(
-            '''CREATE TABLE IF NOT EXISTS logs 
-            (date text, user_id text, is_private integer, text text)''')
-        conn.commit()
-        conn.close()
+        with database_connection(commit=True) as cursor:
+            # Create table logs
+            cursor.execute(
+                '''CREATE TABLE IF NOT EXISTS logs 
+                (date text, user_id text, is_private integer, text text)''')
         logging.info("Database created successfully")
     except Exception as e:
         print(f"Error occurred: {e}")
--- database/utils.py
+++ database/utils.py
@@ -3,75 +3,63 @@ import logging
 
 from datetime import datetime, timedelta
 
+from database.db import database_connection
+
 
 def log_message(message):
     from bot import handler_name
     handler = handler_name.get()
     if handler != '' and handler != 'Stats':
-        conn = sqlite3.connect('db/dnd_bot.db')
-        c = conn.cursor()
-        is_private = 1 if message.chat.id > 0 else 0
+        with database_connection(commit=True) as cursor:
+            is_private = 1 if message.chat.id > 0 else 0
 
-        c.execute("INSERT INTO logs VALUES (?, ?, ?, ?)", (str(datetime.now()), message.from_user.id,
-                                                           int(is_private), handler))
-        conn.commit()
-        conn.close()
-        logging.debug(f"Message logged: {message.text}, handler: {handler}")
+            cursor.execute("INSERT INTO logs VALUES (?, ?, ?, ?)", (str(datetime.now()), message.from_user.id,
+                                                               int(is_private), handler))
+            logging.debug(f"Message logged: {message.text}, handler: {handler}")
 
 
 def get_week_stats():
-    conn = sqlite3.connect('db/dnd_bot.db')
-    c = conn.cursor()
-
-    week_ago = datetime.now() - timedelta(days=7)
-    query = f"""
-    SELECT COUNT(DISTINCT user_id), COUNT(*)
-    FROM logs
-    WHERE date > '{week_ago.strftime('%Y-%m-%d %H:%M:%S')}'
-    """
-    c.execute(query)
-    result = c.fetchone()
-
-    conn.close()
+    with database_connection() as cursor:
+        week_ago = datetime.now() - timedelta(days=7)
+        query = f"""
+        SELECT COUNT(DISTINCT user_id), COUNT(*)
+        FROM logs
+        WHERE date > '{week_ago.strftime('%Y-%m-%d %H:%M:%S')}'
+        """
+        cursor.execute(query)
+        result = cursor.fetchone()
 
     return result
 
 
 def get_month_stats():
-    conn = sqlite3.connect('db/dnd_bot.db')
-    c = conn.cursor()
+    with database_connection() as cursor:
 
-    month_ago = datetime.now() - timedelta(days=30)
-    query = f"""
-    SELECT COUNT(DISTINCT user_id), COUNT(*)
-    FROM logs
-    WHERE date > '{month_ago.strftime('%Y-%m-%d %H:%M:%S')}'
-    """
-    c.execute(query)
-    result = c.fetchone()
-
-    conn.close()
+        month_ago = datetime.now() - timedelta(days=30)
+        query = f"""
+        SELECT COUNT(DISTINCT user_id), COUNT(*)
+        FROM logs
+        WHERE date > '{month_ago.strftime('%Y-%m-%d %H:%M:%S')}'
+        """
+        cursor.execute(query)
+        result = cursor.fetchone()
 
     return result
 
 
 def get_top_5_requests():
-    conn = sqlite3.connect('db/dnd_bot.db')
-    c = conn.cursor()
-
-    week_ago = datetime.now() - timedelta(days=7)
-    query = f"""
-    SELECT text, COUNT(*)
-    FROM logs
-    WHERE date > '{week_ago.strftime('%Y-%m-%d %H:%M:%S')}'
-    GROUP BY text
-    ORDER BY COUNT(*) DESC
-    LIMIT 5
-    """
-    c.execute(query)
-    result = c.fetchall()
-
-    conn.close()
+    with database_connection() as cursor:
+        week_ago = datetime.now() - timedelta(days=7)
+        query = f"""
+        SELECT text, COUNT(*)
+        FROM logs
+        WHERE date > '{week_ago.strftime('%Y-%m-%d %H:%M:%S')}'
+        GROUP BY text
+        ORDER BY COUNT(*) DESC
+        LIMIT 5
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
 
     return result
 
