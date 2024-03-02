7860d8b0fbff011108fefb393e08ddaeaa9d9f58
Andrey <megagran.work@gmail.com>
2024-03-02 16:29:27 +0300

    some beautify

--- src/main.py
+++ src/main.py
@@ -45,6 +45,25 @@ def register_handlers(dp):
         lambda call: call.data.startswith('spell_'),
     )
 
+    dp.register_message_handler(start.start_message, commands=['start'])
+    dp.register_message_handler(start.help_message, commands=['help'])
+    dp.register_message_handler(next_game.set_game, commands=['set'])
+    dp.register_message_handler(next_game.get_game, commands=['game'])
+    dp.register_message_handler(guide.class_search, commands=['class'])
+    dp.register_message_handler(guide.mech_search, commands=['mech'])
+    dp.register_message_handler(guide.item_search, commands=['item'])
+    dp.register_message_handler(create_char.create_character, commands=['create_character'])
+
+    dp.register_message_handler(handlers.bestiary_search.bestiary_search, commands=['bestiary'])
+    dp.register_callback_query_handler(
+        handlers.bestiary_search.beast_callback_query,
+        lambda call: call.data.startswith('beast_'),
+    )
+    dp.register_message_handler(spell_search.spell_search, commands=['spell'])
+    dp.register_callback_query_handler(
+        spell_search.spell_callback_query,
+        lambda call: call.data.startswith('spell_'),
+    )
     dp.register_message_handler(feat_search.feat_search, commands=['feat'])
     dp.register_callback_query_handler(
         feat_search.feat_callback_query,
@@ -63,17 +82,23 @@ def register_handlers(dp):
         lambda c: c.data.startswith('selected_class_'),
     )
     dp.register_callback_query_handler(create_char.select_race, lambda c: c.data == 'select_race')
-    dp.register_callback_query_handler(create_char.select_race_choice,
-                                       lambda c: c.data and c.data.startswith('selected_race_'))
+    dp.register_callback_query_handler(
+        create_char.select_race_choice,
+        lambda c: c.data and c.data.startswith('selected_race_'),
+    )
     dp.register_callback_query_handler(create_char.select_story, lambda c: c.data == 'select_story')
-    dp.register_callback_query_handler(create_char.select_story_choice,
-                                       lambda c: c.data and c.data.startswith('selected_story_'))
+    dp.register_callback_query_handler(
+        create_char.select_story_choice,
+        lambda c: c.data and c.data.startswith('selected_story_'),
+    )
     dp.register_callback_query_handler(
         create_char.select_num_chars,
         lambda c: c.data == 'select_num_chars',
     )
-    dp.register_callback_query_handler(create_char.selected_num_chars,
-                                       lambda c: c.data.startswith('selected_num_chars_'))
+    dp.register_callback_query_handler(
+        create_char.selected_num_chars,
+        lambda c: c.data.startswith('selected_num_chars_'),
+    )
     dp.register_callback_query_handler(create_char.generate, lambda c: c.data == 'generate')
 
     dp.register_message_handler(stats_command, commands=['stats'], user_id=ADMIN_ID)
